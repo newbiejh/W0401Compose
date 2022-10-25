86075199728e249d8b385b9a4f2d4e6cae24afa2
newbiejh <dlwjdgns301@naver.com>
2022-10-25 19:07:31 +0900

    🚀 Row Column 완성

--- app/src/main/java/kr/ac/kumoh/s20180911/w0401compose/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180911/w0401compose/MainActivity.kt
@@ -3,29 +3,67 @@ package kr.ac.kumoh.s20180911.w0401compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kr.ac.kumoh.s20180911.w0401compose.ui.theme.W0401ComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            W0401ComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyApp()
         }
+    }
+}
+
+@Composable
+fun MyApp() {
+    W0401ComposeTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            //Greeting("안녕하세요")
+            MyLinearLayout()
+        }
+    }
+}
+
+@Composable
+fun MyLinearLayout() {
+    Column {
+        Row() {
+            Text(
+                text = "안녕하세요",
+                modifier = Modifier
+                    .background(Color.Yellow)
+                    .padding(8.dp)
+            )
+            Text(
+                text = "컴공",
+                modifier = Modifier
+                    .background(Color.Cyan)
+                    .padding(8.dp)
+            )
+        }
+        Text(
+            text = "어디서 본 것 같은 예제",
+            modifier = Modifier
+                .background(Color.Magenta)
+                .padding(8.dp)
+        )
     }
 }
 
@@ -37,7 +75,5 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    W0401ComposeTheme {
-        Greeting("Android")
-    }
+    MyApp()
 }
